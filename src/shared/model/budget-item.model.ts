1e71d2ee035d9d563c8b43a608f9e9de2c9913b4
Vishakhan MN <32939385+VishakhanMN@users.noreply.github.com>
2021-03-19 14:48:54 +0530

    Add files via upload

---
+++ src/shared/model/budget-item.model.ts
@@ -0,0 +1,12 @@
+export class BudgetItem{
+    // this code is similiar to the uncommented version
+    /* description: string;
+    amount:number;
+
+    constructor(description:string,amount:number){
+        this.description = description;
+        this.amount = amount;
+    }*/
+
+    constructor(public description:string,public amount:any){}
+}
